97802abb18846c110a100d74354d88db461a21c3
jws1837 <jws1837@gmail.com>
2022-06-08 12:08:13 +0900

    수박수박

--- src/test_202206/test_20220608.java
+++ src/test_202206/test_20220608.java
@@ -3,6 +3,8 @@ package test_202206;
 //수박수박수박수박수박수?
 //2022-06-08 11:22 start 2022-06-08 11:28 end
 
+import java.util.stream.IntStream;
+
 /**
  * 문제 설명
  * 길이가 n이고, "수박수박수박수...."와 같은 패턴을 유지하는 문자열을 리턴하는 함수, solution을 완성하세요.
@@ -12,17 +14,28 @@ package test_202206;
  * n은 길이 10,000이하인 자연수입니다.
  **/
 public class test_20220608 {
+
     public String solution(int n) {
         char[] source = {'수', '박'};
         String answer = "";
 
         for (int i = 0; i < n; i++) {
-            int k = 0;
-            k = (i % 2 == 0) ? 0 : 1;
+            int k = (i % 2 == 0) ? 0 : 1;
             answer += source[k];
         }
         return answer;
     }
+
+
+    public String solution2(int n) {
+        StringBuffer sf = new StringBuffer();
+        for (int i = 1; i <= n; ++i) {
+            sf.append(i % 2 == 1 ? "수" : "박");
+        }
+        return sf.toString();
+    }
+
+
 }
 
 
