12960150c5236d760261e9adaaeea1765eef85f0
jws1837 <jws1837@gmail.com>
2022-06-21 16:53:52 +0900

    feat: 나누어 떨어지는 숫자 배열

---
+++ src/test_202206/test_20220621.java
@@ -0,0 +1,53 @@
+package test_202206;
+//나누어 떨어지는 숫자 배열
+//2022-06-21 16:35 start 2022-06-21 16:51 end
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * 문제 설명
+ * array의 각 element 중 divisor로 나누어 떨어지는 값을 오름차순으로 정렬한 배열을 반환하는 함수, solution을 작성해주세요.
+ * divisor로 나누어 떨어지는 element가 하나도 없다면 배열에 -1을 담아 반환하세요.
+ * <p>
+ * 제한사항
+ * arr은 자연수를 담은 배열입니다.
+ * 정수 i, j에 대해 i ≠ j 이면 arr[i] ≠ arr[j] 입니다.
+ * divisor는 자연수입니다.
+ * array는 길이 1 이상인 배열입니다.
+ **/
+public class test_20220621 {
+
+    public int[] solution(int[] arr, int divisor) {
+        Arrays.sort(arr);
+        int[] answer = {};
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i : arr) {
+            if (i % divisor == 0) {
+                list.add(i);
+            }
+        }
+        if (list.size() == 0) {
+            return new int[]{-1};
+        }
+        answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        test_20220621 test = new test_20220621();
+        int[] dd = {3, 2, 6};
+
+        int[] solution = test.solution(dd, 10);
+        for (int i : solution) {
+            System.out.print("i = " + i);
+        }
+    }
+}
+
